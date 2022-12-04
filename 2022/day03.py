3b4f62e9723eba4755c09624139fc2efca5857a8
Blubber <102239406+BlubberIT@users.noreply.github.com>
2022-12-04 18:32:19 +0100

    day03 optimisations

--- 2022/day03.py
+++ 2022/day03.py
@@ -1,12 +1,23 @@
 
-input = map(lambda x: x.strip(), open("input_files/day03.txt", "r").readlines())
-sum=0
+input = list(map(lambda x: x.strip(), open("input_files/day03.txt", "r").readlines()))
 
+def sum_priorities(l):
+    sum_p=0
+    for i in l:
+        if i.isupper():
+            sum_p+=ord(i)-38
+        else:
+            sum_p+=ord(i)-96
+    return sum_p
+
+items=[]
 for rucksack in input:
-    letter=[letter for letter in rucksack[slice(0,len(rucksack)//2)] if letter in rucksack[slice(len(rucksack)//2,len(rucksack))]][0]
-    if letter.isupper():
-        sum+=ord(letter)-38
-    else:
-        sum+=ord(letter)-96
+    items.append([l for l in rucksack[:len(rucksack)//2] if l in rucksack[len(rucksack)//2:]][0])
 
-print("sum of the priorities", sum)
+input_tuples = [tuple(input[i:i+3]) for i in range(0,len(input),3)]
+badges=[]
+for rucksack1,rucksack2,rucksack3 in input_tuples:
+    badges.append([l for l in rucksack1 if l in rucksack2 and l in rucksack3][0])
+
+print("sum of the priorities:", sum_priorities(items))
+print("sum of the priorities (three-Elf group):", sum_priorities(badges))
